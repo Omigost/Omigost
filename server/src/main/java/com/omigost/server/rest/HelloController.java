5fc21d9c180ddfd9b6425ed0e444505a42b4fec2
gor <gorsafaryan1@gmail.com>
2018-11-12 12:51:04 +0100

    Spring Hello World

---
+++ server/src/main/java/com/omigost/server/rest/HelloController.java
@@ -0,0 +1,12 @@
+package com.omigost.server.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public String helloWorld(){
+        return "Good morning, and in case I don't see ya: Good afternoon, good evening, and good night!\n";
+    }
+}
