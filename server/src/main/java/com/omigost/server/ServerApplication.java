019347411f19dd4a8fbe45178e05118c653229ed
gor <gorsafaryan1@gmail.com>
2018-11-12 12:51:04 +0100

    hello world

--- server/src/main/java/com/omigost/server/ServerApplication.java
+++ server/src/main/java/com/omigost/server/ServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
