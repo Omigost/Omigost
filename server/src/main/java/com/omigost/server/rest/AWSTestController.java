f74c9de0b2399fb9dea3bd446c427ce1ded4276c
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2018-11-28 14:34:10 +0100

    Slack rest controller for testing; Removed initial controller

--- server/src/main/java/com/omigost/server/rest/AWSTestController.java
+++ server/src/main/java/com/omigost/server/rest/AWSTestController.java
@@ -2,7 +2,6 @@ package com.omigost.server.rest;
 
 import com.omigost.server.iam.IAMService;
 import com.omigost.server.model.AwsUser;
-import com.omigost.server.slack.SlackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class HelloController {
-    @Autowired
-    private SlackService slack;
+public class AWSTestController {
     @Autowired
     private IAMService iam;
-
-    @GetMapping("/hello")
-    public void helloWorld() {
-        slack.sendAlertToUser("Michał Ołtarzewski", "ALERT TEST");
-    }
 
     @GetMapping("/users")
     public List<AwsUser> users() {
