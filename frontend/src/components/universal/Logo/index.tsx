465f25325f952d4cf5a5439a278ecc18a039d930
styczynski <piotrsty1@gmail.com>
2019-01-02 12:24:01 +0100

    Provide new types for logo component

--- frontend/src/components/universal/Logo/index.tsx
+++ frontend/src/components/universal/Logo/index.tsx
@@ -2,18 +2,43 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 const logo = require('img/omigost_logo.svg');
+const sombrerro = require('img/omigost_sombrerro.svg');
+const pepper = require('img/omigost_single.svg');
 
 const ImgHolder = styled.img`
   width: 100%
 `;
 
+export enum LogoType {
+    Default = 'Default',
+    Sombrerro = 'Sombrerro',
+    Pepper = 'Pepper'
+};
+
 export interface LogoProps {
+    type?: LogoType | string;
 }
 
 export default class Logo extends React.Component<LogoProps, undefined> {
     render() {
+        
+        let src = logo;
+        switch(this.props.type) {
+            case LogoType.Default:
+                src = logo;
+                break;
+            case LogoType.Sombrerro:
+                src = sombrerro;
+                break;
+            case LogoType.Pepper:
+                src = pepper;
+                break;
+            default:
+                src = this.props.type;
+        }
+        
         return (
-            <ImgHolder src={logo} />
+            <ImgHolder src={src} />
         );
     }
 }
