3ee058737ad0aefd4c1112b6e131f62fa497bce8
gor <gorsafaryan1@gmail.com>
2019-01-02 23:47:31 +0400

    fixed frontend conflict

--- server/src/main/java/com/omigost/server/aws/AWSConfig.java
+++ server/src/main/java/com/omigost/server/aws/AWSConfig.java
@@ -16,13 +16,16 @@ public class AWSConfig {
     AWSCredentialsProvider credentials() {
         try {
             EnvironmentVariableCredentialsProvider envCredentials = new EnvironmentVariableCredentialsProvider();
+            envCredentials.getCredentials();//throws exceptions if variables not defined
             log.info("AWS from credentials from Environment variables");
             return envCredentials;
         } catch (SdkClientException ex) {
             log.warn("AWS credentials as environment variables are not set up");
         }
+        PropertiesFileCredentialsProvider credentialsProvider = new PropertiesFileCredentialsProvider("./src/main/resources/application.properties");
+        credentialsProvider.getCredentials();// throws exception if fields not defined
         log.info("AWS from properties");
-        return new PropertiesFileCredentialsProvider("./src/main/resources/application.properties");
+        return credentialsProvider;
     }
 
 
