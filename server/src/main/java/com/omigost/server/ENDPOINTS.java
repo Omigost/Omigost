dab865b947b8aeed03749adcd6417decdb21ea57
mbalc <mbalc909@gmail.com>
2019-02-15 02:12:59 +0100

    Add first version of sns handler service

---
+++ server/src/main/java/com/omigost/server/ENDPOINTS.java
@@ -0,0 +1,10 @@
+package com.omigost.server;
+
+public class ENDPOINTS {
+    public final class SNSHandlers {
+
+        public static final String alertTriggered = "/alerts/trigger";
+
+    }
+
+}
