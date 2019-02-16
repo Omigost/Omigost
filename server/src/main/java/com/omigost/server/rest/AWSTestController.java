61de443722cc07c60920f836c35f0482bd4cccef
gor <gorsafaryan1@gmail.com>
2019-02-16 16:35:34 +0100

    termination, hibernate, stop instance ec2

--- server/src/main/java/com/omigost/server/rest/AWSTestController.java
+++ server/src/main/java/com/omigost/server/rest/AWSTestController.java
@@ -4,8 +4,12 @@ import com.amazonaws.services.budgets.model.Budget;
 import com.amazonaws.services.organizations.model.Account;
 import com.omigost.server.aws.BudgetService;
 import com.omigost.server.aws.OrganizationService;
+import com.omigost.server.aws.TerminationService;
+import com.omigost.server.rest.dto.TerminationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +22,9 @@ public class AWSTestController {
     @Autowired
     private BudgetService budgets;
 
+    @Autowired
+    private TerminationService terminationService;
+
     @GetMapping("/organizations")
     public List<Account> accounts() {
         return organization.getAccounts();
@@ -26,5 +33,20 @@ public class AWSTestController {
     @GetMapping("/budgets")
     public List<Budget> budgets() {
         return budgets.getBudgets();
+    }
+
+    @PostMapping("/hibernate")
+    public void hibernate(@RequestBody TerminationRequest request) {
+        terminationService.hibernate(request.getMachineId());
+    }
+
+    @PostMapping("/stop")
+    public void stop(@RequestBody TerminationRequest request) {
+        terminationService.stop(request.getMachineId());
+    }
+
+    @PostMapping("/terminate")
+    public void terminate(@RequestBody TerminationRequest request) {
+        terminationService.terminate(request.getMachineId());
     }
 }
