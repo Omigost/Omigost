c0d40d3c0b8d9a0f54d9a7f5e47d9a51d254021a
mbalc <mbalc909@gmail.com>
2019-02-21 15:34:03 +0100

    Generate different notifications for forecasted and actual limit violation

--- server/src/main/java/com/omigost/server/notification/MainNotificationService.java
+++ server/src/main/java/com/omigost/server/notification/MainNotificationService.java
@@ -3,6 +3,7 @@ package com.omigost.server.notification;
 import com.amazonaws.services.budgets.model.Budget;
 import com.omigost.server.model.BudgetDecorator;
 import com.omigost.server.model.Communication;
+import com.omigost.server.aws.UserService;
 import com.omigost.server.model.User;
 import com.omigost.server.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,8 @@ public class MainNotificationService {
     @Autowired
     private AccountRepository accountRepository;
 
-    public void alertBudgetOverran(Budget budget) {
-        NotificationMessage message = budgetOverranMessage(budget);
+    public void alertBudgetTriggered(Budget budget) {
+        NotificationMessage message = budgetTriggeredMessage(budget);
         sendBudgetAlert(budget, message);
     }
 
@@ -36,10 +37,36 @@ public class MainNotificationService {
         return result;
     }
 
-    private NotificationMessage budgetOverranMessage(Budget budget) {
-        return NotificationMessage.builder()
-                .mainText("TODO this message should have sth meaningful")
-                .build();
+    private NotificationMessage budgetTriggeredMessage(Budget budget) {
+        if (new BudgetDecorator(budget).isOverrun())
+            return budgetOverrunMessage(budget);
+        return forecastedBudgetOverrunMessage(budget);
+    }
+
+    class RequestLimitIncreaseMessage extends NotificationMessage {
+        RequestLimitIncreaseMessage(String mainText) {
+            super(NotificationMessage.builder()
+                    .mainText(mainText)
+                    // TODO add request text field
+                    .build()
+            );
+        }
+    }
+
+    private NotificationMessage forecastedBudgetOverrunMessage(Budget budget) {
+        return new RequestLimitIncreaseMessage(String.format(
+                "Budget %s is forecasted to exceed its %s limit.",
+                budget.getBudgetName(),
+                budget.getBudgetLimit().getAmount()
+        ));
+    }
+
+    private NotificationMessage budgetOverrunMessage(Budget budget) {
+        return new RequestLimitIncreaseMessage(String.format(
+                "Budget %s has exceeded its %s limit.",
+                budget.getBudgetName(),
+                budget.getBudgetLimit().getAmount()
+        ));
     }
 }
 
