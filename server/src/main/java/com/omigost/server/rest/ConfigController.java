134ac940c2b442ffc883114d32b98eaa05051be3
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-02-22 15:32:06 +0100

    Implemented config endpoints

--- server/src/main/java/com/omigost/server/rest/ConfigController.java
+++ server/src/main/java/com/omigost/server/rest/ConfigController.java
@@ -3,6 +3,7 @@ package com.omigost.server.rest;
 import com.omigost.server.aws.OrganizationService;
 import com.omigost.server.exception.NotFoundException;
 import com.omigost.server.model.Account;
+import com.omigost.server.model.Communication;
 import com.omigost.server.model.User;
 import com.omigost.server.repository.AccountRepository;
 import com.omigost.server.repository.CommunicationRepository;
@@ -10,6 +11,7 @@ import com.omigost.server.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @RestController
 @RequestMapping("/config")
@@ -28,16 +30,23 @@ public class ConfigController {
 
     @PutMapping("/user")
     public void addUser(@RequestParam String name) {
-        User newUser = new User();
-        newUser.name = name;
-
-        userRepository.save(newUser);
+        userRepository.save(new User(name));
     }
 
-    @DeleteMapping("/user")
+    @PostMapping("addCommunicationToUser")
     @Transactional
-    public void deleteUser(@RequestParam String name) {
-        userRepository.deleteUserByName(name);
+    public void addCommunicationToUser(@RequestParam String userName, @RequestParam String communicationName,
+                                       @RequestParam String communicationValue) {
+        Communication communication = new Communication(communicationName, communicationValue);
+        User user = userRepository.getUserByName(userName);
+        if (user == null) {
+            throw new NotFoundException("User by this name could not be found");
+        }
+
+        communication.setUser(user);
+        user.getCommunications().add(communication);
+        communicationRepository.save(communication);
+        userRepository.save(user);
     }
 
     @PostMapping("/addAccountToUser")
@@ -49,12 +58,61 @@ public class ConfigController {
 
         Account account = accountRepository.getOrCreate(accountName);
 
-        User user = userRepository.findUsersByName(userName);
+        User user = userRepository.getUserByName(userName);
         if (user == null) {
             throw new NotFoundException("User by this name could not be found");
         }
 
-        user.accounts.add(account);
+        user.getAccounts().add(account);
+        account.getUsers().add(user);
         userRepository.save(user);
+    }
+
+    @DeleteMapping("/user")
+    @Transactional
+    public void deleteUser(@RequestParam String name) {
+        userRepository.deleteUserByName(name);
+    }
+
+    @DeleteMapping("/deleteUserAccount")
+    @Transactional
+    public void deleteUserAccount(@RequestParam String userName, @RequestParam String accountName) {
+        Account account = accountRepository.getAccountByName(accountName);
+        User user = userRepository.getUserByName(userName);
+
+        if (account == null) {
+            throw new NotFoundException("Account by this name could not be found");
+        } else if (user == null) {
+            throw new NotFoundException("User by this name could not be found");
+        }
+
+        user.getAccounts().remove(account);
+        account.getUsers().remove(user);
+        userRepository.save(user);
+    }
+
+    @DeleteMapping("/deleteUserCommunication")
+    @Transactional
+    public void deleteUserCommunication(@RequestParam String userName, @RequestParam String communicationName,
+                                                @RequestParam String communicationValue) {
+        User user = userRepository.getUserByName(userName);
+        Communication communication =
+                communicationRepository.getCommunicationByNameAndValueAndUser(communicationName, communicationValue, user);
+        if (communication == null) {
+            throw new NotFoundException("Communication by this name, value and username could not be found");
+        }
+
+        communicationRepository.delete(communication);
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/accounts")
+    public List<com.amazonaws.services.organizations.model.Account> getAccounts() {
+        organizationService.fetchAccounts();
+        return organizationService.fetchAccounts();
     }
 }
