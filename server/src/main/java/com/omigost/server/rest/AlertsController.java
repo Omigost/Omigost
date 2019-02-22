eec989a9eb3cc9a3e8e0c87be113069c39d580b0
mbalc <mbalc909@gmail.com>
2019-02-22 17:58:42 +0100

    Rename SnsHandlerController to AlertsController

--- server/src/main/java/com/omigost/server/rest/AlertsController.java
+++ server/src/main/java/com/omigost/server/rest/AlertsController.java
@@ -4,24 +4,26 @@ import com.amazonaws.services.budgets.model.Budget;
 import com.omigost.server.alerts.AlertService;
 import com.omigost.server.aws.BudgetService;
 import com.omigost.server.notification.MainNotificationService;
+import com.omigost.server.rest.exception.BudgetNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SnsHandlerController {
-
+@RequestMapping("/alerts")
+public class AlertsController {
     @Autowired
     private BudgetService budgets;
 
     @Autowired
     private AlertService alerts;
 
-    @PostMapping("/alerts/trigger")
-    public void handleAlertTrigger(@RequestParam String budgetName) {
+    @PostMapping("/trigger")
+    public void handleAlertTrigger(@RequestParam String budgetName) throws BudgetNotFound {
         Budget budget = budgets.getBudgetByName(budgetName);
         if (budget == null) throw new BudgetNotFound();
-        alerts.alertBudgetTriggered(budget);
+        alerts.handleBudgetTriggered(budget);
     }
 }
