84381b435a12758ec5a9e35eeb8c42e4449dce59
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-02-22 16:29:31 +0100

    Implemented endpoints

--- server/src/main/java/com/omigost/server/rest/BudgetController.java
+++ server/src/main/java/com/omigost/server/rest/BudgetController.java
@@ -2,9 +2,9 @@ package com.omigost.server.rest;
 
 import com.amazonaws.services.budgets.model.Budget;
 import com.omigost.server.aws.BudgetService;
+import com.omigost.server.rest.dto.BudgetCreateRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,19 +14,23 @@ public class BudgetController {
     @Autowired
     private BudgetService budgetService;
 
+    @GetMapping
     public List<Budget> getBudgets() {
         return budgetService.getBudgets();
     }
 
-    public void createBudget() {
-
+    @PutMapping("/create")
+    public void createBudget(@RequestBody BudgetCreateRequest budgetCreateRequest) {
+        budgetService.createBudget(budgetCreateRequest.getLimit(), budgetCreateRequest.getLinkedAccounts(), budgetCreateRequest.getTags());
     }
 
-    public void createBudgets() {
-
+    @PutMapping("/createSeparateBudgets")
+    public void createBudgets(@RequestBody BudgetCreateRequest budgetCreateRequest) {
+        budgetService.createSeparateBudgets(budgetCreateRequest.getLimit(), budgetCreateRequest.getLinkedAccounts());
     }
 
-    public void deleteBudget() {
-
+    @DeleteMapping("/delete")
+    public void deleteBudget(@RequestParam String name) {
+        budgetService.deleteBudget(name);
     }
 }
