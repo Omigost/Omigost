c3105b9d612a5d63dbfdc7dfed1d1e9488dc8509
styczynski <piotrsty1@gmail.com>
2019-02-27 16:51:45 +0100

    Automatic tslint code refactor

--- frontend/src/langs/en.ts
+++ frontend/src/langs/en.ts
@@ -1,18 +1,18 @@
 export default {
-  'login_label': 'Login',
-  'login_button_label': 'Login',
-  'reset_password_label': 'I don\'t remeber my password',
-  'password_label': 'Password',
-  'welcome_message_1': 'Hello, how are you today?',
-  'welcome_message_2': 'Always remeber to brush your teeth!',
-  'welcome_message_3': 'Wow, you look so fine today!',
-  'welcome_message_4': 'Can you draw an ellipse by hand?',
-  'login_description': `
+  "login_label": "Login",
+  "login_button_label": "Login",
+  "reset_password_label": "I don't remeber my password",
+  "password_label": "Password",
+  "welcome_message_1": "Hello, how are you today?",
+  "welcome_message_2": "Always remeber to brush your teeth!",
+  "welcome_message_3": "Wow, you look so fine today!",
+  "welcome_message_4": "Can you draw an ellipse by hand?",
+  "login_description": `
 Here you can login to your Omigost account.
 
 We are very glad you decided to use our software.
 
 If you have any troubles with your account,
 please let us know we will investigate your case!
-`
+`,
 };
--- frontend/src/modules/ModulesLoader.ts
+++ frontend/src/modules/ModulesLoader.ts
@@ -1,28 +1,27 @@
-import axios from 'axios'
 
-import { IconName } from '@fortawesome/fontawesome-svg-core';
+import { IconName } from "@fortawesome/fontawesome-svg-core";
 
-import OmigostUI from '../components/universal/index';
+import OmigostUI from "../components/universal/index";
 
-import DashboardViewModule from './DashboardView';
-import SettingsViewModule from './SettingsView';
+import DashboardViewModule from "./DashboardView";
+import SettingsViewModule from "./SettingsView";
 
 const BUILTIN_MODULES: Array<OmigostModule> = [
     new DashboardViewModule(),
-    new SettingsViewModule()
+    new SettingsViewModule(),
 ];
 
 export interface OmigostApp {
     UI: any;
     modulesLoader: OmigostModulesLoaderInterface;
-};
+}
 
 export interface OmigostModuleDetails {
     description?: string;
     author?: string;
     version?: string;
     lastUpdate?: string;
-};
+}
 
 export interface OmigostModule {
     onLoad(app: OmigostApp, loader: OmigostModulesLoaderInterface);
@@ -31,14 +30,14 @@ export interface OmigostModule {
     getDetails(): OmigostModuleDetails;
     renderDashboardView?(props: any): React.ReactElement<any> | null;
     getRoutes?(): Array<OmigostModuleRoute>;
-};
+}
 
 export type ModuleSource = String | OmigostModule;
 
 export interface OmigostModuleRoute {
     name: string;
     component: React.ReactNode;
-};
+}
 
 export interface OmigostModulesLoaderInterface {
     loadModule(src: ModuleSource);
@@ -46,76 +45,76 @@ export interface OmigostModulesLoaderInterface {
     getActiveModules(): Array<OmigostModule>;
     getAllModules(): Array<OmigostModule>;
     getRegisteredRoutes(): Array<OmigostModuleRoute>;
-};
+}
 
 export interface OmigostModuleInstance {
     activated: boolean;
     module: OmigostModule;
-};
+}
 
 export default class OmigostModulesLoader implements OmigostModulesLoaderInterface {
     modules: Array<OmigostModuleInstance>;
-    
+
     constructor() {
         this.modules = [];
     }
-    
+
     getApp(): OmigostApp {
         return {
             UI: OmigostUI,
-            modulesLoader: this
+            modulesLoader: this,
         };
     }
-    
+
     getRegisteredRoutes() {
         let routes: Array<OmigostModuleRoute> = [];
         this.modules.forEach(instance => {
-            if(instance.module.getRoutes) {
+            if (instance.module.getRoutes) {
                 routes = routes.concat(
                     instance.module.getRoutes()
                       .filter(e => !!e)
                       .map(route => {
                           return {
-                              name: '/' + instance.module.getName() + '-' + route.name,
-                              component: route.component
+                              name: "/" + instance.module.getName() + "-" + route.name,
+                              component: route.component,
                           };
-                      })
+                      }),
                 );
             }
         });
-        
+
         return routes;
     }
-    
+
     loadModule(src: ModuleSource) {
         let modInst: OmigostModule = null;
-        if(typeof src === 'string') {
+        if (typeof src === "string") {
             const builtinModulesSearchResult: OmigostModule = BUILTIN_MODULES.filter(module => module.getName() === src)[0];
-            if(builtinModulesSearchResult) {
+            if (builtinModulesSearchResult) {
                 modInst = builtinModulesSearchResult as OmigostModule;
             }
         } else {
             modInst = src as OmigostModule;
         }
-        
-        if(modInst) {
+
+        if (modInst) {
             this.modules.push({
                 activated: true,
-                module: modInst
+                module: modInst,
             });
             modInst.onLoad(this.getApp(), this);
         }
     }
-    
+
     loadAllModules(srcs: Array<ModuleSource>) {
         srcs.forEach(module => this.loadModule(module));
     }
-    
+
     getAllModules() {
         return this.modules.map(instance => instance.module);
     }
-    
+
     getActiveModules() {
         return this.modules.filter(instance => instance.activated).map(instance => instance.module);
     }
-};
+}
