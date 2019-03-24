3a02efb9d1e20c78c7e1f5a732939a245e495f30
styczynski <piotrsty1@gmail.com>
2019-03-24 11:01:21 +0100

    Fix review issues

--- frontend/src/components/universal/Popover/index.tsx
+++ frontend/src/components/universal/Popover/index.tsx
@@ -9,11 +9,11 @@ const PopoverTrigger = styled.div`
 `;
 
 const PopoverContent = styled.div`
-  background:white;
-  box-shadow: 0 0 0 1px rgba(16, 22, 26, 0.1), 0 2px 4px rgba(16, 22, 26, 0.2), 0 8px 24px rgba(16, 22, 26, 0.2);
+  background: white;
+  /* Box shadow is the default style for popover extracted from react-awesome-popover */
+  box-shadow: 0 8px 24px rgba(16, 22, 26, 0.2);
   padding: 10px;
   width: 200px;
-  z-index: 9999999;
 `;
 
 export interface PopoverProps {
