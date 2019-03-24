8195dd1885aff6ecd97198f4837550c0a9b9e0db
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-03-24 10:27:53 +0100

    Run test with embdded h2 db

---
+++ server/src/test/java/com/omigost/server/ServerApplicationTest.java
@@ -0,0 +1,14 @@
+package com.omigost.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ServerApplicationTest {
+
+    @Test
+    public void runAppContextTest() {}
+}
