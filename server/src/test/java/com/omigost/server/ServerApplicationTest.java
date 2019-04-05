753971528016dd43eb9c0ed6bce2f246570d12dc
gor <gorsafaryan1@gmail.com>
2019-04-05 10:17:20 +0200

    test properties

--- server/src/test/java/com/omigost/server/ServerApplicationTest.java
+++ server/src/test/java/com/omigost/server/ServerApplicationTest.java
@@ -3,12 +3,16 @@ package com.omigost.server;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@TestPropertySource("/src/test/resources/test.properties")
 public class ServerApplicationTest {
 
     @Test
-    public void runAppContextTest() {}
+    public void runAppContextTest() {
+
+    }
 }
