82f6fa1a422b43928eb9fb82ec98b532cfac1069
gor <gorsafaryan1@gmail.com>
2019-04-14 18:59:31 +0200

    Encryption works, slack dto, slack response implemented, TODO testing

---
+++ server/src/main/java/com/omigost/server/aws/TokenEncryptingService.java
@@ -0,0 +1,60 @@
+package com.omigost.server.aws;
+
+import lombok.SneakyThrows;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+/**
+ * //TODO temporary solution
+ * The idea is to send the encrypted aws id with the request in base64 format then
+ * when retrieving the token to convert back and stop the machines
+ */
+@Service
+public class TokenEncryptingService {
+
+    private static Cipher cipher;
+    private SecretKey secretKey;
+
+    //TODO fix the key from properties
+    // now fixed during application runtime
+    @PostConstruct
+    @SneakyThrows
+    private void setSecretKey() {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("DESede");
+        // keysize must be equal to 112 or 168 for this provider
+        keyGenerator.init(168);
+        secretKey = keyGenerator.generateKey();
+        cipher = Cipher.getInstance("DESede");
+    }
+
+    @SneakyThrows
+    public String encryptMessage(String message) {
+        byte[] messageInBytes = message.getBytes("UTF8");
+        byte[] encryptedMaessage = encrypt(messageInBytes, secretKey);
+        return Base64.encodeBase64String(encryptedMaessage);
+    }
+
+    @SneakyThrows
+    public String descryptMessage(String encMessage) {
+        byte[] received = Base64.decodeBase64(encMessage);
+        byte[] decryptedBytes = decrypt(received, secretKey);
+        return new String(decryptedBytes, "UTF8");
+    }
+
+    @SneakyThrows
+    private byte[] encrypt(byte[] plainTextByte, SecretKey secretKey) {
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return cipher.doFinal(plainTextByte);
+    }
+
+    @SneakyThrows
+    private byte[] decrypt(byte[] encryptedBytes, SecretKey secretKey) {
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return cipher.doFinal(encryptedBytes);
+    }
+}
--- server/src/main/java/com/omigost/server/aws/termination/ScheduledNotificationService.java
+++ server/src/main/java/com/omigost/server/aws/termination/ScheduledNotificationService.java
@@ -3,24 +3,32 @@ package com.omigost.server.aws.termination;
 import com.amazonaws.services.organizations.model.Account;
 import com.omigost.server.aws.MachineListingService;
 import com.omigost.server.aws.OrganizationService;
+import com.omigost.server.aws.TokenEncryptingService;
 import com.omigost.server.model.Communication;
 import com.omigost.server.notification.NotificationMessage;
 import com.omigost.server.notification.slack.SlackMessageAction;
 import com.omigost.server.notification.slack.SlackService;
 import com.omigost.server.repository.AccountRepository;
 import com.omigost.server.repository.CommunicationRepository;
+import com.omigost.server.rest.dto.SlackResponse.Action;
+import com.omigost.server.rest.dto.SlackResponse.SlackResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 /**
  * Terminates machines when running out of business hours
  */
 @Service
+@RestController
+@RequestMapping("/slackResponseHandler")
 public class ScheduledNotificationService {
     @Autowired
     EC2TerminationService ec2TerminationService;
@@ -40,28 +48,57 @@ public class ScheduledNotificationService {
     @Autowired
     CommunicationRepository communicationRepository;
 
+    @Autowired
+    TokenEncryptingService tokenEncryptingService;
+
+    private final String positiveActionValue = "yes";
+    private final String negativeActionValue = "no";
+
     //        @Scheduled(cron = "30 18-23/1,0-8/1 * * *")
     @Scheduled(cron = "1/1 * * * * ?")
     void notifyOutOfBusinessHours() {
         List<Account> accountList = organizationService.fetchAccounts();
         for (Account account : accountList) {
-            if(!account.getEmail().contains("test")) continue;
+            if (!account.getEmail().contains("test")) {
+                continue;
+            }
             List<String> runningEC2Instances = machineListingService.getRunningEC2Instances(account.getId());
-            if (runningEC2Instances.size() > 0) sendSlackReminder(account, runningEC2Instances.size());
+            if (runningEC2Instances.size() > 0) {
+                sendSlackReminder(account, runningEC2Instances.size());
+            }
         }
     }
 
     void sendSlackReminder(Account account, int instanceCount) {
         Collection<Communication> communications = communicationRepository.getCommunicationsForAWSId(account.getId());
+        String callbackId = tokenEncryptingService.encryptMessage(account.getId());
+
         for (Communication communication : communications) {
             NotificationMessage notificationMessage = NotificationMessage.builder()
                     .mainText("There are " + instanceCount + " EC2 instances running out of business hours.")
                     .mainText("Do you want me to stop them ?")
-                    .action(new SlackMessageAction("ec2ResponseAction", "Yes", "yes"))
-                    .action(new SlackMessageAction("ec2ResponseAction", "No", "no"))
+                    .action(new SlackMessageAction("ec2ResponseAction", "Yes", positiveActionValue))
+                    .action(new SlackMessageAction("ec2ResponseAction", "No", negativeActionValue))
                     .build();
-            slackService.sendAlertToUser(communication, notificationMessage);
+
+            slackService.sendAlertToUser(communication, notificationMessage, callbackId);
         }
     }
 
+    private void terminateRunningUserInsatnace(String awsUserId) {
+        List<String> runningEC2Instances = machineListingService.getRunningEC2Instances(awsUserId);
+        ec2TerminationService.terminate(runningEC2Instances, awsUserId);
+    }
+
+    @PostMapping
+    private void slackRespnonseHandler(@RequestBody SlackResponseDTO responseDTO) {
+        Action action = responseDTO.getActions().get(0);
+        String callBackId = responseDTO.getCallback_id();
+        String awsUserId = tokenEncryptingService.descryptMessage(callBackId);
+
+        if (action.getValue().equals(positiveActionValue)) {
+            terminateRunningUserInsatnace(awsUserId);
+        }
+
+    }
 }
