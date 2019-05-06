c12f5705eabf6b289c88879a883c1a493cb196b4
styczynski <piotrsty1@gmail.com>
2019-05-07 00:44:09 +0100

    Add deployment docker-compose development local mode

--- deployment/deploy.py
+++ deployment/deploy.py
@@ -5,11 +5,12 @@ import os.path
 from scripts.logger import createLogger, setLoggerLevel
 from scripts.generate_config import generateConfig
 from scripts.ebcli import runEBCli
+from scripts.composecli import runComposeCli
 
 @click.command()
 @click.argument('profile')
-@click.option('--keyEnv', '-k', default='AWS_KEY', help='Environment variable that stores AWS key')
-@click.option('--secretEnv', '-s', default='AWS_SECRET', help='Environment variable that stores AWS key')
+@click.option('--keyEnv', '-k', default='AWS_KEY', help='Environment variable that stores AWS key (default: AWS_KEY)')
+@click.option('--secretEnv', '-s', default='AWS_SECRET', help='Environment variable that stores AWS key (default: AWS_SECRET)')
 @click.option('--path', '-p', default='.', help='Path to deployment profiles directory')
 @click.option('--loggingLevel', '-l', default='INFO', type=click.Choice(['INFO', 'DEBUG']), help='Set logging level')
 @click.option('--outputJSON', '-o', default='Dockerrun.aws.json', help='File to store Dockerrun.aws.json config')
@@ -19,21 +20,35 @@ from scripts.ebcli import runEBCli
 @click.option('--newEnvInstance', '-i', default='t3.medium', help="Instance type for which new environment is created")
 @click.option('--newEnvOptions', '-envopt', default='[]', help="Additional options passed to EB Cli when creating new environment")
 @click.option('--cli', '-c', default=False, help='Enable non-interactive CLI mode')
+@click.option('--devCompose', '-d', default=False, help='Runs Omigost locally using pure local sources and Docker composer (disables all deployment features)')
 def runCli(
         profile, keyenv, secretenv, path, logginglevel, outputjson, dry,
-        newenvname, createnewenv, newenvinstance, newenvoptions, cli
+        newenvname, createnewenv, newenvinstance, newenvoptions, cli,
+        devcompose
     ):
+    configMode = 'aws'
     logger = createLogger('deploy.py')
     logger = setLoggerLevel(logger, logginglevel)
 
+    if devcompose:
+        logger.info('Development docker-compose mode is on. Deployment will not take place. Instead of that compose will be launched with Omigost based on local sources.')
+        logger.info('--devCompose was specified: Using --outputJSON docker-compose.dev.yml override.')
+        outputjson = "docker-compose.dev.yml"
+        configMode = 'compose-dev'
+
     with open('{}/{}'.format(path, outputjson), 'w') as the_file:
-        the_file.write(generateConfig(profile, path=path, logger=logger, secretEnv=secretenv, keyEnv=keyenv))
+        the_file.write(generateConfig(profile, path=path, logger=logger, secretEnv=secretenv, keyEnv=keyenv, mode=configMode))
     if dry:
         logger.info('Dry mode is active. Nothing to do.')
         return
 
-    if createnewenv:
+    if devcompose:
+        logger.info('Development docker-compose mode is on. Deployment will not take place. Instead of that compose will be launched with Omigost based on local sources.')
+        #runComposeCli(['-f', outputjson, 'rm', '--force'], path=path, logger=logger)
+        runComposeCli(['-f', outputjson, 'up', '--force-recreate', '--build'], path=path, logger=logger)
+        return
 
+    if (not devcompose) and createnewenv:
         hasEBDirectory = os.path.exists('{}/.elasticbeanstalk'.format(path))
         if not hasEBDirectory:
             logger.info('EB configuration was not detected. EB will be initialized.')
@@ -65,22 +80,23 @@ def runCli(
             runEBCli(createEnvCommand, path=path, logger=logger)
             runEBCli(['use', newenvname], path=path, logger=logger)
 
-    statusOutput = runEBCli(['status'], path=path, logger=logger, collectOutput=True).splitlines()
-    cnameUrl = None
-    for line in statusOutput:
-        lineTokens = line.split(':')
-        if len(lineTokens) == 2:
-            varName = lineTokens[0].replace(' ', '')
-            varValue = lineTokens[1].replace(' ', '')
-            if varName == 'CNAME':
-                cnameUrl = varValue
+    if (not devcompose):
+        statusOutput = runEBCli(['status'], path=path, logger=logger, collectOutput=True).splitlines()
+        cnameUrl = None
+        for line in statusOutput:
+            lineTokens = line.split(':')
+            if len(lineTokens) == 2:
+                varName = lineTokens[0].replace(' ', '')
+                varValue = lineTokens[1].replace(' ', '')
+                if varName == 'CNAME':
+                    cnameUrl = varValue
 
-    if not (cnameUrl is None):
-        logger.info('\n   ---> Deployed application to http://{}/\n'.format(cnameUrl))
-        if not cli:
-            try:
-                webbrowser.open_new_tab('http://{}/'.format(cnameUrl))
-            except:
-                doNothing = True
+        if not (cnameUrl is None):
+            logger.info('\n   ---> Deployed application to http://{}/\n'.format(cnameUrl))
+            if not cli:
+                try:
+                    webbrowser.open_new_tab('http://{}/'.format(cnameUrl))
+                except:
+                    doNothing = True
 
 runCli()
--- deployment/scripts/generate_config.py
+++ deployment/scripts/generate_config.py
@@ -1,16 +1,30 @@
 import json
+import yaml
 import os
 from container_transform.compose import ComposeTransformer
 from container_transform.ecs import ECSTransformer
 
-def generateConfig(envName='dev', path='.', logger=None, secretEnv=None, keyEnv=None):
-    configInputPath = '{}/{}/docker-compose.yml'.format(path, envName)
-    if logger:
-        logger.info('Reading configuration from {} file'.format(configInputPath))
+def generateConfig(envName='dev', path='.', logger=None, secretEnv=None, keyEnv=None, mode='aws'):
+    if mode == 'aws':
+        return generateConfigAWS(
+            envName=envName,
+            path=path,
+            logger=logger,
+            secretEnv=secretEnv,
+            keyEnv=keyEnv
+        )
+    elif mode == 'compose-dev':
+        return generateConfigComposeDev(
+            envName=envName,
+            path=path,
+            logger=logger,
+            secretEnv=secretEnv,
+            keyEnv=keyEnv
+        )
+    else:
+        raise Exception('Invalid mdoe was specified for generateConfig. Got {}, expected: aws or compose-dev'.format(mode))
 
-    transformer = ComposeTransformer(configInputPath)
-    normalized_keys = transformer.ingest_containers()
-
+def loadSecrets(secretEnv, keyEnv):
     awsKey = None
     awsSecret = None
 
@@ -26,6 +40,85 @@ def generateConfig(envName='dev', path='.', logger=None, secretEnv=None, keyEnv=
 
     if (awsKey is None) or (awsSecret is None):
         raise Exception('Could not find AWS key/secret in environment variables nor in CLI argument')
+
+    return {
+        "awsKey": awsKey,
+        "awsSecret": awsSecret
+    }
+
+
+def generateConfigComposeDev(envName='dev', path='.', logger=None, secretEnv=None, keyEnv=None):
+    configInputPath = '{}/{}/docker-compose.yml'.format(path, envName)
+    if logger:
+        logger.info('Reading configuration from {} file'.format(configInputPath))
+
+    transformer = ComposeTransformer(configInputPath)
+    normalized_keys = transformer.ingest_containers()
+
+    secrets = loadSecrets(secretEnv, keyEnv)
+    awsKey = secrets["awsKey"]
+    awsSecret = secrets["awsSecret"]
+
+    services = {}
+    for service in normalized_keys:
+        services[service["name"]] = service
+        service.pop("name", None)
+    normalized_keys = services
+
+    omigostService = normalized_keys["omigost"]
+    omigostService["volumes"] = [ "..:/opt/app/omigost" ]
+    omigostService["image"] = "openjdk:11"
+    springArgs=" ".join([
+        "--spring.datasource.url=jdbc:postgresql://postgres:5432/docker_dev",
+        "--spring.datasource.username=admin",
+        "--spring.datasource.password=admin1",
+        "--localstack.postgres.useExternal=true",
+        "--localstack.services.useExternal=true",
+        "--localstack.services.ip=localstack",
+        "--localstack.services.sns.port=4575",
+        "--localstack.services.dumb.port=4567",
+        "--localstack.localAWSBudgets.useExternal=true",
+        "--localstack.localAWSBudgets.ip=budgets",
+        "--localstack.localAWSBudgets.port=5000",
+        "--localstack.motocker.iam.useExternal=true",
+        "--localstack.motocker.iam.ip=iam",
+        "--localstack.motocker.iam.port=5000",
+        "--localstack.motocker.ec2.useExternal=true",
+        "--localstack.motocker.ec2.ip=ec2",
+        "--localstack.motocker.ec2.port=5000",
+        "--localstack.motocker.organizations.useExternal=true",
+        "--localstack.motocker.organizations.ip=organizations",
+        "--localstack.motocker.organizations.port=5000"
+    ])
+
+    commandStr = ("./gradlew bootRun -PspringArgs='{}' && ".format(springArgs) * 50) + "echo FINISHED"
+    omigostService["command"] = "sh -c \"cd /opt/app/omigost && {}\"".format(commandStr)
+    omigostService["environment"] = [
+        "AWS_ACCESS_KEY={}".format(awsKey),
+        "AWS_SECRET_KEY={}".format(awsSecret)
+    ]
+    omigostService["ports"].append("8100:8100")
+
+    compose_config = {
+        "version": "3",
+        "services": normalized_keys
+    }
+
+    if logger:
+        logger.info('Generated configuration from {} file'.format(configInputPath))
+    return yaml.dump(compose_config, default_flow_style=False)
+
+def generateConfigAWS(envName='dev', path='.', logger=None, secretEnv=None, keyEnv=None):
+    configInputPath = '{}/{}/docker-compose.yml'.format(path, envName)
+    if logger:
+        logger.info('Reading configuration from {} file'.format(configInputPath))
+
+    transformer = ComposeTransformer(configInputPath)
+    normalized_keys = transformer.ingest_containers()
+
+    secrets = loadSecrets(secretEnv, keyEnv)
+    awsKey = secrets["awsKey"]
+    awsSecret = secrets["awsSecret"]
 
     global_envs = [{
         'name': 'AWS_ACCESS_KEY',
