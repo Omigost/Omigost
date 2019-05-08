1c0360c9ef6eb719a01e4423b19fd8d2aea80353
mbalc <mbalc909@gmail.com>
2019-05-09 01:59:09 +0200

    Add suggestions for future for Alert model

--- server/src/main/java/com/omigost/server/model/Alert.java
+++ server/src/main/java/com/omigost/server/model/Alert.java
@@ -16,10 +16,10 @@ public class Alert {
     public Integer id;
 
     @ManyToOne
-    @JoinColumn(name="communication", referencedColumnName="id")
+    @JoinColumn(name="communication", referencedColumnName="id") // TODO do we really need these duplications for something?
     public Communication communication;
 
-    @ManyToOne
+    @ManyToOne // TODO consider multiple tokens per alert (ie. expiring and reissuing tokens)
     @JoinColumn(name="alert_response_token", referencedColumnName="id")
     public AlertResponseToken token;
 }
