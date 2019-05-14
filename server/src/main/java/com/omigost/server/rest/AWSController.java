a5c7af8ba14bad767d13e001316f9a7d1308d3c1
gor <gorsafaryan1@gmail.com>
2019-05-14 17:18:26 +0200

    fix account spending

--- server/src/main/java/com/omigost/server/rest/AWSController.java
+++ server/src/main/java/com/omigost/server/rest/AWSController.java
@@ -1,9 +1,10 @@
 package com.omigost.server.rest;
 
 import com.amazonaws.services.organizations.model.Account;
+import com.omigost.server.aws.CostService;
 import com.omigost.server.aws.OrganizationService;
 import com.omigost.server.aws.termination.ScheduledTerminationService;
-import com.omigost.server.rest.dto.TerminationCronDTO;
+import com.omigost.server.rest.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,8 @@ public class AWSController {
     private OrganizationService organization;
     @Autowired
     private ScheduledTerminationService terminationService;
+    @Autowired
+    private CostService costService;
 
     @GetMapping("/organizations")
     public List<Account> accounts() {
@@ -38,6 +41,17 @@ public class AWSController {
         String withoutLastComma = stringBuilder.toString().substring(0, stringBuilder.length() - 1);
         String cron = String.format("0 0/%d %s * * *", cronDTO.getPeriod(), withoutLastComma);
         terminationService.scheduleNotificationService(cron);
+    }
+
+
+    @PostMapping("/spending/account")
+    public List<AWSDailySpendingDTO> spending(@RequestBody AccountSpendingDTO request) {
+        return costService.getSpendingForAccount(request.getDateInterval(), request.getUserId());
+    }
+
+    @PostMapping("/spending/tag")
+    public List<AWSDailySpendingDTO> tagSpending(@RequestBody TagSpendingDTO request) {
+        return costService.getSpendingForTags(request.getDateInterval(), request.getTags());
     }
 
 }
