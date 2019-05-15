82a60e5aa7fa1610218ed92c949e93ef04925205
styczynski <piotr@styczynski.in>
2019-05-15 16:46:31 +0100

    Fix demo controller import

--- server/src/main/java/com/omigost/server/rest/demo/DemoController.java
+++ server/src/main/java/com/omigost/server/rest/demo/DemoController.java
@@ -1,22 +1,12 @@
 package com.omigost.server.rest.demo;
 
-import com.amazonaws.services.budgets.model.Budget;
 import com.amazonaws.services.organizations.AWSOrganizations;
 import com.amazonaws.services.organizations.model.CreateAccountRequest;
 import com.amazonaws.services.organizations.model.CreateAccountResult;
 import com.amazonaws.services.organizations.model.CreateOrganizationRequest;
 import com.amazonaws.services.organizations.model.ListAccountsRequest;
-import com.omigost.server.alerts.AlertService;
-import com.omigost.server.aws.BudgetService;
-import com.omigost.server.model.AlertResponseToken;
-import com.omigost.server.notification.MainNotificationService;
-import com.omigost.server.rest.dto.RequestLimitIncreaseRequest;
-import com.omigost.server.rest.dto.SubscriptionConfirmationRequest;
-import com.omigost.server.rest.exception.BudgetNotFound;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/demo")
